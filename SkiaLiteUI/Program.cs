574261769dc10a31f4d941911534de0bd2ea6848
kyle <kongpob.gafe@g.swu.ac.th>
2026-02-13 16:45:42 +0700

    save

---
+++ SkiaLiteUI/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace SkiaLiteUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            new WinTest().Run();
+            //Console.WriteLine("Hello, World!");
+        }
+    }
+}
